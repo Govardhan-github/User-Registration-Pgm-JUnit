cb103af135fa9927db7cf6ed289a622df5b1943f
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-08-05 10:34:03 -0700

    ADDED : Refactoring The Code By Using Custom Exception

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -10,47 +10,75 @@ public class UserRegistration {
     The Pattern Defines The First Name Starts With Capital Letter
     The Name Should Have Min 3 Words
      */
-    public boolean validateFirstName(String firstName){
+    public boolean validateFirstName(String firstName) throws UserRegistrationException {
         String Name_Pattern = "^[A-Z]{1}[a-z]{2,}$";//The First Letter With Cap And Min 3 Letters
-        return pattern.matches(Name_Pattern, firstName);//Matching The Name Pattern And FirstName And Returning It
+        boolean match =  pattern.matches(Name_Pattern, firstName);//Matching The Name Pattern And FirstName And Returning It
+        if(match) {
+            System.out.println("First Name Is Valid");
+        } else{
+            throw new UserRegistrationException("Invalid First Name");
+        }
+        return match;
     }
     /*
     Declaring Validate Last Name Method
     The Pattern Defines The Last Name Starts With Capital Letter
     The Name Should Have Min 3 Words
     */
-    public boolean validateLastName(String LastName) {
-        //Here Declaring The Name Pattern
+    public boolean validateLastName(String LastName) throws UserRegistrationException {
         String Name_pattern = "^[A-Z]{1}[a-z]{2,}$";//The First Letter With Cap And Min 3 Letters
-        return pattern.matches(Name_pattern , LastName);//Matching0 The Name Pattern And LastName And Returning It.
+        boolean match = pattern.matches(Name_pattern , LastName);//Matching0 The Name Pattern And LastName And Returning It.
+        if(match) {
+            System.out.println("Last Name Is Valid");
+        } else{
+            throw new UserRegistrationException("Invalid Last Name");
         }
+        return match;
+    }
     /*
     Declaring Valid Email Method
     The Email Should Starts With Small Letter
     Here Declaring The Email Pattern
     */
-    public boolean validateemail(String Email) {
+    public boolean validateemail(String Email) throws UserRegistrationException {
         String Email_pattern = "[a-z][a-zA-Z0-9]+([._+#-][a-zA-Z0-9]+)*@[a-zA-Z]{5}+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
-        return pattern.matches(Email_pattern,Email);//Matching The Email Pattern And Email And Returning It.
+        boolean match = pattern.matches(Email_pattern,Email);//Matching The Email Pattern And Email And Returning It.
+        if(match) {
+            System.out.println("Email Is Valid");
+        } else{
+            throw new UserRegistrationException("Invalid Email Id");
+        }
+        return match;
+
         }
     /*
     Declaring Valid Phone Number Method
     The Phone Number Pattern Contain The Phone Should Have 10 Numbers
     Here Declaring The Phone Number Pattern
     */
-    public boolean validatePhoneNumber(String PhoneNumber) {
-        //Here Declaring The Phone Number Pattern
+    public boolean validatePhoneNumber(String PhoneNumber) throws UserRegistrationException {
         String Number_pattern = "^[9][1][ ][6-9]{1}[0-9]{9}$";
-        return pattern.matches(Number_pattern, PhoneNumber);//Matching The Phone Number Pattern And Phone Number And Returning It
+        boolean match = pattern.matches(Number_pattern, PhoneNumber);//Matching The Phone Number Pattern And Phone Number And Returning It
+        if(match) {
+            System.out.println("Phone Number Is Valid");
+        } else{
+            throw new UserRegistrationException("Invalid Phone Number");
         }
+        return match;
+    }
     /*
     Declaring Valid Password Method
     The Password Should Contain The Minimum Of 8 Characters
     Here Declaring The Password Pattern
     */
-    public boolean validatePassword(String Password) {
-        //Here Declaring The Password Pattern
+    public boolean validatePassword(String Password) throws UserRegistrationException {
         String Password_pattern = "^(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%!]).{8,}";
-        return pattern.matches(Password_pattern,Password);
+        boolean match= pattern.matches(Password_pattern,Password);
+        if(match) {
+            System.out.println("Password Is Valid");
+        } else{
+            throw new UserRegistrationException("Invalid Password");
+        }
+        return match;
     }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -7,30 +7,38 @@ import org.junit.jupiter.api.Test;
 public class UserRegistrationTest {
     //Defining User Registration
     UserRegistration userRegistration = new UserRegistration();
-
     //Test To Valid FirstName Starts With Cap Letter And It Has Min 3 Letters
     @Test
     /*
     Validation Of First Name And Return True
+    Here Declaring Try Catch Block For Exception Handling
     Checking Validate FirstName With Pattern And Storing In result
     Asserting The Result As Boolean Type True Or False
     */
-    public void givenFirstName_Return_True() {
-        //Checking Validate FirstName With Pattern And Storing In result
-        boolean result = userRegistration.validateFirstName("Gopi");
-        System.out.println(result);
-        //Asserting The Result As Boolean Type True Or False
-        Assertions.assertEquals(true, result);
+    public void givenFirstName_StartsWithCapLetter_Return_True() {
+        try {
+            //First Name Is Valid It Returns Valid Message
+            boolean result = userRegistration.validateFirstName("Gopi");
+            System.out.println(result);
+            Assertions.assertEquals(true, result);
+        }catch(UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
     /*
     Test To Valid FirstName If It Starts With Small Letter Print False
     Checking Validate FirstName With Pattern And Storing In result
+    If FirstName Is Not Match With Pattern It Throws Exception Handling Message
     Asserting The Result As Boolean Type True Or False
     */
     @Test
-    public void givenFirstName_Return_False() throws UserRegistrationException {
-        boolean result = userRegistration.validateFirstName("gopi");
-        Assertions.assertEquals(false, result);
+    public void givenFirstName_StartsWithSmallLetter_Return_False() {
+        try {
+            boolean result = userRegistration.validateFirstName("gopi");
+            Assertions.assertEquals(false, result);
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
     }
     //UC2
     /*
@@ -39,20 +47,31 @@ public class UserRegistrationTest {
     Asserting The Result As Boolean Type True Or False
     */
     @Test
-    public void givenLastName_Return_True() {
-        userRegistration.validateLastName("Reddy");
-        boolean result = userRegistration.validateFirstName("Reddy");
-        Assertions.assertEquals(true, result);
+    public void givenLastName_StartsWithCapLetter_Return_True()  {
+        try {
+            boolean result = userRegistration.validateLastName("Reddy");
+            Assertions.assertEquals(true, result);
+            System.out.println(result);
+        }catch(UserRegistrationException e){
+            e.printStackTrace();
+
+        }
     }
     @Test
     /*
      Test To Valid LastName Starts With Small Letter And Return False
      Checking Validate LastName With Pattern And Storing In result
+     If LastName Is Not Match With Pattern It Throws Exception Handling Message
      Asserting The Result As Boolean Type True Or False
     */
-    public void givenLastName_Return_False() {
-        boolean result = userRegistration.validateLastName("reddy");
-        Assertions.assertEquals(false, result);
+    public void givenLastName_StartsWithSmallLetter_Return_False() {
+        try {
+            boolean result = userRegistration.validateLastName("reddy");
+            Assertions.assertEquals(false, result);
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
+
     }
     //UC3
     /*
@@ -62,18 +81,29 @@ public class UserRegistrationTest {
     */
     @Test
     public void givenEmail_StartsWithSmallLetter_ReturnTrue() {
-        boolean result = userRegistration.validateemail("gopi1998@gmail.com");
-        Assertions.assertEquals(true, result);
+        try {
+            boolean result = userRegistration.validateemail("gopi1998@gmail.com");
+            Assertions.assertEquals(true, result);
+            System.out.println(result);
+        }catch(UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
     /*
     Test To Valid Email Starts With Cap Letter And Return False
     Checking Validate Email With Pattern And Storing In result
+    If Email Is Not Match With Pattern It Throws Exception Handling Message
     Asserting The Result As Boolean Type True Or False
     */
     @Test
     public void givenEmail_StartsWithCapLetter_ReturnFalse() {
-        boolean result = userRegistration.validateemail("Gopi1998@gmail.com");
-        Assertions.assertEquals(false, result);
+        try {
+            boolean result = userRegistration.validateemail("Gopi1998@gmail.com");
+            Assertions.assertEquals(false, result);
+            System.out.println(result);
+        }catch(UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
     /*
     Test To Valid Email Ends With .Com Return True
@@ -82,8 +112,13 @@ public class UserRegistrationTest {
     */
     @Test
     public void givenEmail_EndsWithCom_ReturnTrue() {
-        boolean result = userRegistration.validateemail("gopi1998@gmail.com");
-        Assertions.assertEquals(true, result);
+        try {
+            boolean result = userRegistration.validateemail("gopi1998@gmail.com");
+            Assertions.assertEquals(true, result);
+            System.out.println(result);
+        }catch(UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
     //UC4
     /*
@@ -93,18 +128,30 @@ public class UserRegistrationTest {
     */
     @Test
     public void givenPhoneNumber_StartWithCountryCode_ReturnTrue() {
-        boolean result = userRegistration.validatePhoneNumber("91 9666110767");
-        Assertions.assertEquals(true, result);
+        try {
+            boolean result = userRegistration.validatePhoneNumber("91 9666110767");
+            Assertions.assertEquals(true, result);
+            System.out.println(result);
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
+
     }
     /*
     Test To Phone Number Does Not Have 10Numbers Return False
     Checking Validate Phone Number With Pattern And Storing In result
+    If Phone Number Is Not Match With Pattern It Throws Exception Handling Message
     Asserting The Result As Boolean Type True Or False
     */
     @Test
     public void givenPhoneNumber_DoesNotHave10Numbers_ReturnFalse() {
-        boolean result = userRegistration.validatePhoneNumber("91 966611077");
-        Assertions.assertEquals(false, result);
+        try {
+            boolean result = userRegistration.validatePhoneNumber("91 966611077");
+            Assertions.assertEquals(false, result);
+            System.out.println(result);
+        }catch(UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
     /*
     Test To Phone Number Does Have 10Numbers Return True
@@ -113,8 +160,14 @@ public class UserRegistrationTest {
     */
     @Test
     public void givenPhoneNumber_DoesHave10Numbers_ReturnTrue() {
-        boolean result = userRegistration.validatePhoneNumber("91 9666110767");
-        Assertions.assertEquals(true, result);
+        try {
+
+            boolean result = userRegistration.validatePhoneNumber("91 9666110767");
+            Assertions.assertEquals(true, result);
+            System.out.println(result);
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
     //UC5
     /*
@@ -124,18 +177,29 @@ public class UserRegistrationTest {
     */
     @Test
     public void givenPassword_ContainsEightDigits_ReturnTrue() {
-        boolean result = userRegistration.validatePassword("Gopi$145");
-        Assertions.assertEquals(true,result);
+        try {
+            boolean result = userRegistration.validatePassword("Gopi$145");
+            Assertions.assertEquals(true, result);
+            System.out.println(result);
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
     /*
     Test To Password Does Not Have 8 Characters Return False
     Checking Validate Password With Pattern And Storing In result
+    If Password Is Not Match With Pattern It Throws Exception Handling Message
     Asserting The Result As Boolean Type True Or False
     */
     @Test
     public void givenPassword_DoesNotContainsEightDigits_ReturnFalse() {
-        boolean result = userRegistration.validatePassword("gopi$144");
-        Assertions.assertEquals(false,result);
+        try {
+            boolean result = userRegistration.validatePassword("gopi$144");
+            Assertions.assertEquals(false, result);
+            System.out.println(result);
+        }catch(UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
     //UC6
     /*
@@ -146,18 +210,30 @@ public class UserRegistrationTest {
     */
     @Test
     public void givenPassword_ShouldHaveOneUpperCase_AndContainsEightDigits_ReturnTrue() {
-        boolean result = userRegistration.validatePassword("Gopi$144");
-        Assertions.assertEquals(true,result);
+        try {
+            boolean result = userRegistration.validatePassword("Gopi$144");
+            Assertions.assertEquals(true, result);
+            System.out.println(result);
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
     /*
     Test To Password Does Not Have UpperCase And 8 Characters Return False
     Checking Validate Password With Pattern And Storing In result
+    If Password Is Not Match With Pattern It Throws Exception Handling Message
     Asserting The Result As Boolean Type True Or False
     */
     @Test
     public void givenPassword_ShouldNotHaveOneUpperCase_AndContainsEightDigits_ReturnFalse() {
-        boolean result = userRegistration.validatePassword("gopi$144");
-        Assertions.assertEquals(false,result);
+        try {
+            boolean result = userRegistration.validatePassword("gopi$144");
+            Assertions.assertEquals(false, result);
+            System.out.println(result);
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
+
     }
     //UC7
     /*
@@ -168,18 +244,29 @@ public class UserRegistrationTest {
     */
     @Test
     public void givenPassword_ShouldHaveOneNumericNbr_AndContainsEightDigits_ReturnTrue() {
-        boolean result = userRegistration.validatePassword("Gopi$144");
-        Assertions.assertEquals(true,result);
+        try {
+            boolean result = userRegistration.validatePassword("Gopi$144");
+            Assertions.assertEquals(true, result);
+            System.out.println(result);
+        }catch(UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
     /*
     Test To Password Does Not Have One Numeric Number And 8 Characters Return True
     Checking Validate Password With Pattern And Storing In result
+    If Password Is Not Match With Pattern It Throws Exception Handling Message
     Asserting The Result As Boolean Type True Or False
     */
     @Test
     public void givenPassword_ShouldNotHaveOneNumericNbr_AndContainsEightDigits_ReturnFalse() {
-        boolean result = userRegistration.validatePassword("Gopi$rdy");
-        Assertions.assertEquals(false,result);
+        try {
+            boolean result = userRegistration.validatePassword("Gopi$rdy");
+            Assertions.assertEquals(false, result);
+            System.out.println(result);
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
     /*
     UC8 Password Should Contain One Special Character
@@ -189,17 +276,28 @@ public class UserRegistrationTest {
     */
     @Test
     public void givenPassword_ShouldHaveOneSpecialCharacter_AndContainsEightDigits_ReturnTrue() {
-        boolean result = userRegistration.validatePassword("Gopi$144");
-        Assertions.assertEquals(true,result);
+        try {
+            boolean result = userRegistration.validatePassword("Gopi$144");
+            Assertions.assertEquals(true, result);
+            System.out.println(result);
+        }catch(UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
     /*
     Test To Password Does Not Have One Numeric Number And 8 Characters Return True
     Checking Validate Password With Pattern And Storing In result
+    If Password Is Not Match With Pattern It Throws Exception Handling Message
     Asserting The Result As Boolean Type True Or False
     */
     @Test
     public void givenPassword_ShouldNotHaveOneSpecialCharacter_AndContainsEightDigits_ReturnFalse() {
-        boolean result = userRegistration.validatePassword("Gopiredy");
-        Assertions.assertEquals(false,result);
+        try {
+            boolean result = userRegistration.validatePassword("Gopiredy");
+            Assertions.assertEquals(false, result);
+            System.out.println(result);
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
     }
 }
